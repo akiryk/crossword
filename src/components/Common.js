97f8fcfefbfdd9e81b2fb3eae1c252b51eee3528
Adam Kiryk <akiryk@wayfair.com>
2022-04-09 06:32:40 -0600

    enable keyboard movement

---
+++ src/components/Common.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+export const Button = styled.button`
+  border: 1px solid green;
+  padding: 8px 16px;
+`;
