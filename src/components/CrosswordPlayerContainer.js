b02673d72108ff181428f60b0e977453a821aaa8
Adam Kiryk <akiryk@wayfair.com>
2022-04-16 10:05:54 -0600

    start thinking about next cell in play mode

--- src/components/CrosswordPlayerContainer.js
+++ src/components/CrosswordPlayerContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import CrosswordPlayer from "./CrosswordPlayer";
 import { useCrosswordContext } from "../context/CrosswordContextProvider";
+import { BLACK_CELL } from "../utils/constants";
 
 function CrosswordPlayerContainer() {
   const { grid } = useCrosswordContext();
@@ -8,7 +9,22 @@ function CrosswordPlayerContainer() {
     grid.unsetValues();
   }, [grid]);
 
-  const goToNextCell = () => {};
+  const goToNextCell = ({ row, column }) => {
+    const current = grid.cellsObject[`${column}:${row}`];
+    let i = 1;
+    let next = grid.cellsObject[`${column + i}:${row}`];
+    // while (next.displayState === BLACK_CELL) {
+    //   i++;
+    //   next = grid.cellsObject[`${column + i}:${row}`];
+    // }
+    if (!next) {
+      // go to the next column or row
+    } else if (next?.displayState === BLACK_CELL) {
+      console.log("next", next);
+    } else {
+      console.log(next);
+    }
+  };
   return (
     <div className="CrosswordWrapper">
       <h2>Play the game</h2>
