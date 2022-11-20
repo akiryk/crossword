6d056eedd8c3f40f7dc287055de1f4f352dd0eb1
Adam Kiryk <akiryk@wayfair.com>
2022-11-20 10:37:43 -0500

    bring all movement logic to cell container

--- src/components/CellContainer.js
+++ src/components/CellContainer.js
@@ -9,6 +9,8 @@ import {
   PLAY_MODE,
   VIEW_ONLY_MODE,
   DEAD_CELL_MODE,
+  SPAN,
+  HIGHEST_INDEX,
 } from "../utils/constants";
 import { useGridContext } from "../context/GridProvider";
 
@@ -23,7 +25,6 @@ const SPACEBAR_KEY = "SPACE";
 function CellContainer({
   row,
   column,
-  goToNextCell,
   cell,
   displayNumber,
   setCellWithFocus,
@@ -53,6 +54,131 @@ function CellContainer({
     });
     // }
   }, [cellSubscribe]);
+
+  function getCellBelow({ currentRow, currentColumn }) {
+    let newRow = currentRow;
+    let newColumn = currentColumn;
+    if (currentRow < SPAN - 1) {
+      newRow = currentRow + 1;
+    } else {
+      newRow = 0;
+      newColumn = currentColumn + 1 === SPAN ? 0 : currentColumn + 1;
+    }
+
+    const possibleNextCell = grid.cellsObject[`${newColumn}:${newRow}`];
+    while (shouldSkipNextCell(possibleNextCell)) {
+      newRow = currentRow + 1;
+      return getCellBelow({
+        currentRow: newRow,
+        currentColumn: newColumn,
+      });
+    }
+    return { row: newRow, column: newColumn };
+  }
+
+  function getCellAbove({ currentRow, currentColumn, overrideDirectionMode }) {
+    let newRow = SPAN;
+    if (currentRow > 0) {
+      newRow = currentRow - 1;
+    }
+    return { row: newRow, column: currentColumn };
+  }
+
+  function shouldSkipNextCell(cell, overrideDirectionMode) {
+    if (cell.mode === PLAY_MODE && !overrideDirectionMode) {
+      const emptyCellsRemain = Object.values(grid.workingAnswersKey).includes(
+        null
+      );
+      return cell.mode === DEAD_CELL_MODE || (!!cell.value && emptyCellsRemain);
+    }
+    return cell.mode === DEAD_CELL_MODE;
+  }
+
+  function getCellToTheRight({
+    currentRow,
+    currentColumn,
+    overrideDirectionMode,
+  }) {
+    let newColumn = currentColumn + 1;
+    let newRow = currentRow;
+    if (newColumn >= SPAN) {
+      newColumn = 0;
+      newRow = currentRow + 1 === SPAN ? 0 : currentRow + 1;
+    }
+    const possibleNextCell = grid.cellsObject[`${newColumn}:${newRow}`];
+    while (shouldSkipNextCell(possibleNextCell, overrideDirectionMode)) {
+      return getCellToTheRight({
+        currentRow: newRow,
+        currentColumn: newColumn,
+      });
+    }
+    return { row: newRow, column: newColumn };
+  }
+
+  function getCellToTheLeft({
+    currentRow,
+    currentColumn,
+    isDelete,
+    overrideDirectionMode,
+  }) {
+    let nextColumn = currentColumn;
+    let nextRow = currentRow;
+    if (currentColumn > 0) {
+      nextColumn = currentColumn - 1;
+    } else {
+      nextColumn = HIGHEST_INDEX; // HIGHEST_INDEX = SPAN - 1;
+      nextRow = currentRow - 1 < 0 ? HIGHEST_INDEX : currentRow - 1;
+    }
+    const possibleNextCell = grid.cellsObject[`${nextColumn}:${nextRow}`];
+    while (shouldSkipNextCell(possibleNextCell, overrideDirectionMode)) {
+      return getCellToTheLeft({
+        currentRow: nextRow,
+        currentColumn: nextColumn,
+      });
+    }
+    return { row: nextRow, column: nextColumn };
+  }
+
+  function goToNextCell({
+    row,
+    column,
+    isDelete = false,
+    overrideDirectionMode = "",
+  }) {
+    let nextCellFunction = () => {};
+    let direction = overrideDirectionMode
+      ? overrideDirectionMode
+      : grid.gridDirection;
+    if (grid.gridDirection === GO_LEFT_TO_RIGHT && isDelete) {
+      direction = GO_RIGHT_TO_LEFT;
+    }
+    if (grid.gridDirection === GO_TOP_TO_BOTTOM && isDelete) {
+      direction = GO_BOTTOM_TO_TOP;
+    }
+    switch (direction) {
+      case GO_RIGHT_TO_LEFT:
+        nextCellFunction = getCellToTheLeft;
+        break;
+      case GO_LEFT_TO_RIGHT:
+        nextCellFunction = getCellToTheRight;
+        break;
+      case GO_BOTTOM_TO_TOP:
+        nextCellFunction = getCellAbove;
+        break;
+      case GO_TOP_TO_BOTTOM:
+        nextCellFunction = getCellBelow;
+        break;
+      default:
+        break;
+    }
+    const nextCell = nextCellFunction({
+      currentRow: row,
+      currentColumn: column,
+      isDelete,
+    });
+
+    setCellWithFocus(`${nextCell.column}:${nextCell.row}`);
+  }
 
   function handleChange(event) {
     // if (cellIsInteractive) {
@@ -88,22 +214,11 @@ function CellContainer({
         } else if (cellMode === EDIT_MODE) {
           grid.ensureRotationalSymmetry(cell);
         }
-        if (grid.gridDirection === GO_LEFT_TO_RIGHT) {
-          goToNextCell({
-            row,
-            column,
-            overrideDirectionMode: GO_RIGHT_TO_LEFT,
-            isDelete: true,
-          });
-        }
-        if (grid.gridDirection === GO_TOP_TO_BOTTOM) {
-          goToNextCell({
-            row,
-            column,
-            overrideDirectionMode: GO_BOTTOM_TO_TOP,
-            isDelete: true,
-          });
-        }
+        goToNextCell({
+          row,
+          column,
+          isDelete: true,
+        });
         break;
       case LEFT_ARROW_KEY:
         goToNextCell({ row, column, overrideDirectionMode: GO_RIGHT_TO_LEFT });
